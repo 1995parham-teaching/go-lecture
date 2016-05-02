2cb01699845123ca5376a042b83bc4115722ca84
Parham Alvani <parham.alvani@gmail.com>
2016-05-02 12:30:40 +0430

    Hello world sample code was added.

---
+++ linuxfest/hello.go
@@ -0,0 +1,16 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 02-05-2016
+ * |
+ * | File Name:     hello.go
+ * +===============================================
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, World\n")
+}
