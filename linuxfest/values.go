e95bdbb468958286611e8d27303bc8f08f4cfa41
Parham Alvani <parham.alvani@gmail.com>
2016-05-10 08:04:23 +0400

    Values sample code was added.

---
+++ linuxfest/values.go
@@ -0,0 +1,18 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 10-05-2016
+ * |
+ * | File Name:     values.go
+ * +===============================================
+ */
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 10
+	fmt.Printf("%d\n", a)
+}
