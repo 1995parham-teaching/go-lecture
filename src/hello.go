f1819d8809a754aab0e831c6544c1caa4d5e9300
Parham Alvani <parham.alvani@gmail.com>
2016-05-11 21:09:36 +0430

    Source codes headers were removed.

--- src/hello.go
+++ src/hello.go
@@ -1,12 +1,3 @@
-/*
- * +===============================================
- * | Author:        Parham Alvani (parham.alvani@gmail.com)
- * |
- * | Creation Date: 02-05-2016
- * |
- * | File Name:     hello.go
- * +===============================================
- */
 package main
 
 import "fmt"
