24358f501be39fef24e31668efb87e14b2b816d9
Parham Alvani <parham.alvani@gmail.com>
2016-05-12 20:37:48 +0430

    Methods sample code was improved.

--- src/methods.go
+++ src/methods.go
@@ -19,4 +19,22 @@ func (e *Example) Log() {
 }
 
 func main() {
+	var i integer
+
+	var exm1 Example
+	exm1.Val = "Example 1"
+	exm1.count = 0
+
+	exm2 := Example{
+		Val:   "Example 2",
+		count: 10,
+	}
+
+	i.log()
+
+	exm1.Log()
+	exm1.Log()
+
+	exm2.Log()
+	exm2.Log()
 }
