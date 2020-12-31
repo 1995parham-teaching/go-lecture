c73f1b673ba3badbf8badabeb2c889d765103ec5
Parham Alvani <parham.alvani@gmail.com>
2020-12-31 23:44:52 +0330

    feat: Add Loops Example

--- 04-loops/main.go
+++ 04-loops/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+func GCD(n, m int) int {
+	for n%m != 0 {
+		n, m = m, n%m
+	}
+
+	return m
+}
+
+func NthPrime(n int) int {
+	i := 2
+	counter := 0
+
+	for {
+		if IsPrime(i) {
+			counter++
+
+			if counter == n {
+				return i
+			}
+		}
+
+		i++
+	}
+}
+
 func IsPrime(n int) bool {
 	for i := 2; i < n; i++ {
 		if n%i == 0 {
@@ -15,7 +40,13 @@ func IsPrime(n int) bool {
 func main() {
 	var n int
 
+	fmt.Print("please enter n: ")
 	fmt.Scanf("%d", &n)
 
-	fmt.Printf("is %d prime? %t", n, IsPrime(n))
+	fmt.Printf("is %d prime? %t\n", n, IsPrime(n))
+
+	fmt.Printf("%dth prime is %d\n", n, NthPrime(n))
+
+	fmt.Println("gcd", 10, 20, "is", GCD(10, 20))
+	fmt.Println("gcd", 13, 15, "is", GCD(13, 15))
 }
