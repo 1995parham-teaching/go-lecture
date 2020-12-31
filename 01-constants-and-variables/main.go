5e5bfc6bd76f0e3d95dd5c355117c2db86959995
Parham Alvani <parham.alvani@gmail.com>
2020-12-31 17:57:45 +0330

    feat: Improve Variables Example

--- 01-constants-and-variables/main.go
+++ 01-constants-and-variables/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+const (
+	c1       = 10.1
+	c2 int64 = 11
+)
+
+var v1 = 10
+var v2 uint64 = 10
+
 func main() {
 	x := 10
 
 	var y int
 
 	fmt.Println(x)
-
 	fmt.Printf("%d\n", y)
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Printf("%d %d\n", v1, v2)
 }
