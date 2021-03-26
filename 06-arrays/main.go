62876d056fdc85be5c16437c55147e3f7a202ed5
Parham Alvani <parham.alvani@gmail.com>
2021-03-27 01:37:51 +0430

    feat: Add Array Example

---
+++ 06-arrays/main.go
@@ -0,0 +1,29 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 27-03-2021
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var a [10]int
+	var b = [...]int{1, 2, 3, 4}
+
+	for index, value := range a {
+		fmt.Printf("a[%d] = %d\n", index, value)
+	}
+
+	for index, value := range b {
+		fmt.Printf("b[%d] = %d\n", index, value)
+	}
+}
