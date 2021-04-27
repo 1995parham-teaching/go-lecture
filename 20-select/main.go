4570b3baec0c4b8a4988a2c52dfab2f420d253b7
Parham Alvani <parham.alvani@gmail.com>
2021-04-27 15:01:04 +0430

    feat: Add Select Example

---
+++ 20-select/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+func wait() chan int {
+	ch := make(chan int)
+
+	go func() {
+		r := rand.Intn(10)
+		time.Sleep(time.Duration(r) * time.Second)
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func main() {
+	ch1 := wait()
+	ch2 := wait()
+	ch3 := wait()
+
+	select {
+	case <-ch1:
+		log.Println(1)
+	case <-ch2:
+		log.Println(2)
+	case <-ch3:
+		log.Println(3)
+	default:
+		// other cases must have results in the moment or the default case will be exectued
+		log.Println("Failed")
+		return
+	}
+
+	log.Println("Success")
+}
