0a216285999d0b7d87bb59dbd47345dc4ea3ff53
Parham Alvani <parham.alvani@gmail.com>
2021-12-06 07:36:46 +0330

    feat: update pipeline example

--- 20-pipeline/main.go
+++ 20-pipeline/main.go
@@ -3,28 +3,49 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+const processRoutines = 2
 
 func main() {
 	var ch chan int
 	ch = make(chan int)
 
 	// create 2 process to process data from the channel
-	for id := 0; id < 2; id++ {
+	for id := 0; id < processRoutines; id++ {
 		go func(id int) {
 			// each processor prints the data besides its id
 			for {
-				i := <-ch
-				fmt.Printf("Process %d in %d\n", i, id)
+				i, ok := <-ch
+				if !ok {
+					// if channel is closed, we will close the processor
+					fmt.Printf("closed the processosr %d\n", id)
+					return
+				}
+				fmt.Printf("process %d in %d\n", i, id)
 			}
 		}(id)
 	}
 
 	// produce data evey 1 second into the channel
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println("Input")
-		ch <- rand.Intn(10)
-	}
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("we have a new input")
+			ch <- rand.Intn(10)
+		}
+	}()
+
+	// wait for termination signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	close(ch)
+
+	// how we can wait for processors to quit?
 }
