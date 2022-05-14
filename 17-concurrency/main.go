45ba99468fa67379ce64793d58eafd7f09c0031f
Parham Alvani <parham.alvani@gmail.com>
2022-05-14 13:06:23 +0430

    feat: better naming on goroutines

--- 17-concurrency/main.go
+++ 17-concurrency/main.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 
 	go func() {
-		fmt.Println("chan-2")
+		fmt.Println("goroutine-2")
 	}()
 
-	fmt.Println("chan-1")
+	fmt.Println("goroutine-1")
 
 	// block until goroutine execuation
 	// everything will be killed when main died
