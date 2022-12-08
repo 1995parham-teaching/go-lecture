c9db182aa667d1952d8cae58b5aabc55000ae191
Parham Alvani <parham.alvani@gmail.com>
2022-12-08 08:45:32 +0330

    feat: buffered and unbuffered names for channel

--- 19-channels/1/main.go
+++ 19-channels/1/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// make(chan int, 10) with 10 rooms
-	ch := make(chan int) // with 0 room
+	// make(chan int, 10) with 10 rooms (buffered channel)
+	ch := make(chan int) // with 0 room (unbuffered channel)
 
 	go func() {
 		for i := 0; i < 10; i++ {
