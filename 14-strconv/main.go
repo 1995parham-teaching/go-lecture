97a74a76aa78d88245775e45187444e484f654d2
Parham Alvani <parham.alvani@gmail.com>
2024-04-16 13:59:36 +0000

    feat: update strconv example

--- 14-strconv/main.go
+++ 14-strconv/main.go
@@ -9,6 +9,8 @@ func main() {
 	s := "1230"
 	a := 10
 
+	// second argument indicates base and it doesn't
+	// accept 0x even when you have 16 as base.
 	i, _ := strconv.ParseInt(s, 10, 32)
 
 	// compile error: mismatched types int64 and int
@@ -21,4 +23,22 @@ func main() {
 
 	// print multiple return values with println
 	fmt.Println(strconv.Atoi("123"))
+
+	// Generic ParseInt which is written by me!
+	y, _ := ParseInt[int]("562", 16)
+	fmt.Println(y)
+	fmt.Println(y + a)
+}
+
+type Number interface {
+	int | int32 | int64 | int16 | int8
+}
+
+func ParseInt[T Number](s string, b int) (T, error) {
+	v, err := strconv.ParseInt(s, b, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return T(v), nil
 }
